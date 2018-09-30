001c639ec7887988359376a3e355f947b7decfcd
leifarne <leif.arne.rones@gmail.com>
2018-09-30 19:02:00 +0200

    Battery icons selected between the interval from 3.0v to 1.7v.

--- main.js
+++ main.js
@@ -16,17 +16,37 @@ function handleQueryBatteryLevelResponse(response) {
     var battery_level = data.getValue(0,0)
     console.log(battery_level)
 
+ /*    3	100
+    2.87	90
+    2.74	80
+    2.61	70
+    2.48	60
+    2.35	50
+    2.22	40
+    2.09	30
+    1.96	20
+    1.83	10
+    1.7	0 */
+
     var battery_level_icon = ""
-    if (battery_level > 2.0) {
+    if (battery_level > 2.9) {
         battery_level_icon = "svg/baseline-battery_full-24px.svg"
-    } else if (battery_level > 1.9) {
+    } else if (battery_level > 2.87) {
         battery_level_icon = "svg/baseline-battery_90-24px.svg"
-    } else if (battery_level > 1.8) {
+    } else if (battery_level > 2.74) {
         battery_level_icon = "svg/baseline-battery_80-24px.svg"
-    } else if (battery_level > 1.7) {
+    } else if (battery_level > 2.48) {
         battery_level_icon = "svg/baseline-battery_60-24px.svg"
-    } else if (battery_level > 1.6) {
+    } else if (battery_level > 2.35) {
         battery_level_icon = "svg/baseline-battery_50-24px.svg"
+    } else if (battery_level > 2.22) {
+        battery_level_icon = "svg/baseline-battery_40-24px.svg"
+    } else if (battery_level > 2.09) {
+        battery_level_icon = "svg/baseline-battery_30-24px.svg"
+    } else if (battery_level > 1.96) {
+        battery_level_icon = "svg/baseline-battery_20-24px.svg"
+    } else if (battery_level > 1.83) {
+        battery_level_icon = "svg/baseline-battery_10-24px.svg"
     } else {
         battery_level_icon = "svg/baseline-battery_alert-24px.svg"
     }
@@ -34,7 +54,6 @@ function handleQueryBatteryLevelResponse(response) {
     var battery_id = document.getElementById("battery")
     battery_id.setAttribute("src", battery_level_icon)
 }
-
 
 
 function drawTemperatureChart() {
